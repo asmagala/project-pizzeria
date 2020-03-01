6c684639a63b9af997915c2235a15a3da4619f5a
Arkadiusz Smagała <asmagala@outlook.com>
2020-03-01 21:23:32 +0100

    Create DatePicker.js, add it to Booking.js, add css and js files for range-slider to index.html.

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
 import {templates, select} from '../settings.js';
+import DatePicker from './DatePicker.js';
 import AmountWidget from './AmountWidget.js';
 
 class Booking {
@@ -19,6 +20,7 @@ class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
   }
 
   initWidgets() {
@@ -26,6 +28,7 @@ class Booking {
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker =new DatePicker(thisBooking.dom.datePicker);
   }
 }
 
---
+++ src/js/components/DatePicker.js
@@ -0,0 +1,54 @@
+/* global flatpickr */
+
+import BaseWidget from './BaseWidget.js';
+import {utils} from '../utils.js';
+import {select, settings} from '../settings.js';
+
+class DatePicker extends BaseWidget {
+  constructor(wrapper) {
+    super(wrapper, utils.dateToStr(new Date()));
+    const thisWidget = this;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+    thisWidget.initPlugin();
+  }
+
+  initPlugin() {
+    const thisWidget = this;
+
+    thisWidget.minDate = new Date(thisWidget.value);
+    /* Set maxDate that is maxDaysInFuture days later than minDate */
+    thisWidget.maxDate = new Date(utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture));
+    /* Initialize flatpickr with options */
+    flatpickr(thisWidget.dom.input, {
+      defaultDate: thisWidget.minDate,
+      minDate: thisWidget.minDate,
+      maxDate: thisWidget.maxDate,
+      disable: [
+        function(date) {
+          return (date.getDay() === 1 );
+        }
+      ],
+      onChange: function(dateStr) {
+        thisWidget.value = dateStr;
+      },
+      locale: {
+        firstDayOfWeek: 1,
+      }
+    });
+  }
+
+  parseValue(properDate) {
+    return properDate;
+  }
+
+  isValid() {
+    return true;
+  }
+
+  renderValue() {
+    /* empty function to overwrite base function */
+  }
+}
+
+export default DatePicker;
